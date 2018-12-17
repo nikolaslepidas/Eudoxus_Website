40c5d761e0458f9a1e657114a1b214ec17bb48d5
Nikolas Lep <nikolaslep_@hotmail.com>
2018-12-17 17:18:05 +0200

    almost ready breadcrumbs

--- testPage.php
+++ testPage.php
@@ -4,21 +4,21 @@
 	<head>
 		<charset ="UTF-8">
 		<title>Title</title>
-		
+		<link rel="stylesheet" href="css/base.css">
 		<link rel="stylesheet" href="css/breadcrumbs.css">
 	</head>
 	<body>
-		
+
+	<?php require_once './header.php' ?>
+	<div class="wrapper">
 		<ul class="breadcrumb">
 			<li><a href="#">Page1</a></li>
 			<li><a href="#">Page2</a></li>
 			<li><a href="#">Page3</a></li>
 			<li>Current Page</li> 
 		</ul>
-		
-		
-		
-	
+	</div>
+	<?php require_once './footer.php' ?>
 
     </body>
 </html>
