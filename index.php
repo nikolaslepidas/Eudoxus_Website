2511bbc8d2e56faf584b241ab5cbf8d0b447d3b0
ejernity <nikoslamprinos13@gmail.com>
2018-12-18 12:18:46 +0200

    eikones_diafimiseis

--- index.php
+++ index.php
@@ -49,76 +49,56 @@
 	<div id="persons-table">
 		<table>
 			<tbody>
-			<tr>
-				<td><img src="images/person_student.png"></td>
-				<td><img src="images/person_publisher.png"></td>
-				<td><img src="images/person_ordering.png"></td>
-				<td><img src="images/person_secretariat.png"></td>
-			</tr>
-			<tr id="test">
-				<td>Φοιτητές</td>
-				<td>Εκδόσεις</td>
-				<td>Σημεία διανομής</td>
-				<td>Γραμματεία</td>
-			</tr>
-			<tr>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-			</tr>
-			<tr>
-				<td>Δήλωση συγγραμμάτων</td>
-				<td>Διανομή συγγραμμάτων</td>
-				<td>Παράδοση συγγραμμάτων</td>
-				<td>Εισαγωγή προγράμματος σπουδών</td>
-			</tr>
-			<tr>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-				<td></td>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-				<td><a href="#"><img src="images/dilosi_sigrammaton.png"></a></td>
-			</tr>
-			<tr>
-				<td>Ανταλλαγή συγγραμμάτων</td>
-				<td></td>
-				<td>Παραγγελία συγγραμμάτων</td>
-				<td>Εισαγωγή προτεινόμενων συγγραμμάτων</td>
-			</tr>
-		</tbody>
+				<tr id="persons_img">
+					<td><img src="images/person_student.png"></td>
+					<td><img src="images/person_publisher.png"></td>
+					<td><img src="images/person_ordering.png"></td>
+					<td><img src="images/person_secretariat.png"></td>
+				</tr>
+				<tr id="fonts">
+					<td>Φοιτητές</td>
+					<td>Εκδόσεις</td>
+					<td>Σημεία διανομής</td>
+					<td>Γραμματεία</td>
+				</tr>
+				<tr id="icons">
+					<td><a href="#"><img src="icons/pencil-writing-on-a-paper-sheet.png"></a></td>
+					<td><a href="#"><img src="icons/delivery-van.png"></a></td>
+					<td><a href="#"><img src="icons/book.png"></a></td>
+					<td><a href="#"><img src="icons/hand-writing-with-ballpen.png"></a></td>
+				</tr>
+				<tr id="fonts">
+					<td>Δήλωση συγγραμμάτων</td>
+					<td>Διανομή συγγραμμάτων</td>
+					<td>Παράδοση συγγραμμάτων</td>
+					<td>Εισαγωγή προγράμματος σπουδών</td>
+				</tr>
+				<tr id="icons">
+					<td><a href="#"><img src="icons/books-stack-of-three.png"></a></td>
+					<td></td>
+					<td><a href="#"><img src="icons/pencil-writing-on-a-paper-sheet.png"></a></td>
+					<td><a href="#"><img src="icons/checklist.png"></a></td>
+				</tr>
+				<tr id="fonts">
+					<td>Ανταλλαγή συγγραμμάτων</td>
+					<td></td>
+					<td>Παραγγελία συγγραμμάτων</td>
+					<td>Εισαγωγή προτεινόμενων συγγραμμάτων</td>
+				</tr>
+			</tbody>
 		</table>
 	</div>
 </div>
 
 
 <div id="right-column">
-	<div >
-		<table id="newstable">
-			<tr>
-				<th>
-					Διαφημίσεις
-				</th>
-			</tr>
-			<tr>
-				<td><a href="#">Πρόβλημα λειτουργίας της εφαρμογής εκδοτών μέσω Mozilla Firefox.</a></td>
-			</tr>
-			<tr>
-				<td><p>Σας ενημερώνουμε οτι λόγω απροσδόκητου προβλήματος που οφείλεται σε νεότερη έκδοση του περιηγητή Mozilla Firefox, η χρήση της ...</p></td>
-			</tr>
-			<tr>
-				<td><a href="#">Πρόβλημα λειτουργίας της εφαρμογής εκδοτών μέσω Mozilla Firefox.</a></td>
-			</tr>
-			<tr>
-				<td><p>Σας ενημερώνουμε οτι λόγω απροσδόκητου προβλήματος που οφείλεται σε νεότερη έκδοση του περιηγητή Mozilla Firefox, η χρήση της ...</p></td>
-			</tr>
-			<tr>
-				<td><a href="#">Πρόβλημα λειτουργίας της εφαρμογής εκδοτών μέσω Mozilla Firefox.</a></td>
-			</tr>
-			<tr>
-				<td><p>Σας ενημερώνουμε οτι λόγω απροσδόκητου προβλήματος που οφείλεται σε νεότερη έκδοση του περιηγητή Mozilla Firefox, η χρήση της ...</p></td>
-			</tr>
-		</table>
-</div>
+	<div>
+		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
+		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
+		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
+		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
+		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
+	</div>
 </div>	
 
 
