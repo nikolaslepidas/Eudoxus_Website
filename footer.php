3f1da445ac7e40221a4f60e6100d6668c080e3c9
ejernity <nikoslamprinos13@gmail.com>
2018-12-18 11:09:44 +0200

    epanafora

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!--	Footer	-->
+<div class="wrapper">
+<div id="footer">
+		<div ><a href="#" >Συχνές Ερωτήσεις</a></div>
+		<div > <img src="images/logo.png">  </div>
+		<div ><a href="#" >Ηλ. Φόρμα Επικοινωνίας</a></div>
+		<div > <img src="images/logo.png">  </div>
+		<div ><a href="#" >Τηλέφωνο Επικοινωνίας</a></div>
+		<div > <img src="images/logo.png">  </div>
+</div>
+</div>
+<!--	End of Footer	-->
