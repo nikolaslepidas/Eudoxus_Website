4f62a3a5f173ee61d4f9f9f16472774f4f7a4752
ejernity <nikoslamprinos13@gmail.com>
2018-12-21 15:01:05 +0200

    new home page

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	<?php require_once './header.php' ?>
 
 <!-- Newstable -->
-<div id="left-column">
+<div id="right-column">
 	<div id="news-table">
 		<table id="newstable">
 			<tr>
@@ -49,65 +49,46 @@
 				
 <div id="central-column">
 	<div id="persons-table">
-		<table>
-			<tbody>
-				<tr id="persons_img">
-					<td><img src="images/person_student.png"></td>
-					<td><img src="images/person_publisher.png"></td>
-					<td><img src="images/person_ordering.png"></td>
-					<td><img src="images/person_secretariat.png"></td>
-				</tr>
-				<tr id="fonts">
-					<td>Φοιτητές</td>
-					<td>Εκδόσεις</td>
-					<td>Σημεία διανομής</td>
-					<td>Γραμματεία</td>
-				</tr>
-				<tr id="icons">
-					<td><a href="#"><img src="icons/pencil-writing-on-a-paper-sheet.png"></a></td>
-					<td><a href="#"><img src="icons/delivery-van.png"></a></td>
-					<td><a href="#"><img src="icons/book.png"></a></td>
-					<td><a href="#"><img src="icons/hand-writing-with-ballpen.png"></a></td>
-				</tr>
-				<tr id="fonts">
-					<td>Δήλωση συγγραμμάτων</td>
-					<td>Διανομή συγγραμμάτων</td>
-					<td>Παράδοση συγγραμμάτων</td>
-					<td>Εισαγωγή προγράμματος σπουδών</td>
-				</tr>
-				<tr id="icons">
-					<td><a href="#"><img src="icons/books-stack-of-three.png"></a></td>
-					<td></td>
-					<td><a href="#"><img src="icons/pencil-writing-on-a-paper-sheet.png"></a></td>
-					<td><a href="#"><img src="icons/checklist.png"></a></td>
-				</tr>
-				<tr id="fonts">
-					<td>Ανταλλαγή συγγραμμάτων</td>
-					<td></td>
-					<td>Παραγγελία συγγραμμάτων</td>
-					<td>Εισαγωγή προτεινόμενων συγγραμμάτων</td>
-				</tr>
-			</tbody>
-		</table>
+		<div id="up">
+			<a href="students.php"></a>
+			<a href="students.php"></a>
+		</div>
+		<div id="down">
+			<a href="students.php"></a>
+			<a href="students.php"></a>
+		</div>
 	</div>
 </div>
 
-
-<div id="right-column">
-	<div id="adverts">
-		<p>Χορηγοί</p>
-		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
-		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
-		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
-		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
-		<a href="#"><img src="./icons/EurywhereLogo.png"></a>
-	</div>
-</div>	
-
-
-
 <?php require_once './footer.php'  ?>
 
+
+<!--
+<tr id="icons">
+	<td><a href="#"><img src="icons/pencil-writing-on-a-paper-sheet.png"></a></td>
+	<td><a href="#"><img src="icons/delivery-van.png"></a></td>
+	<td><a href="#"><img src="icons/book.png"></a></td>
+	<td><a href="#"><img src="icons/hand-writing-with-ballpen.png"></a></td>
+</tr>
+<tr id="fonts">
+	<td>Δήλωση συγγραμμάτων</td>
+	<td>Διανομή συγγραμμάτων</td>
+	<td>Παράδοση συγγραμμάτων</td>
+	<td>Εισαγωγή προγράμματος σπουδών</td>
+</tr>
+<tr id="icons">
+	<td><a href="#"><img src="icons/books-stack-of-three.png"></a></td>
+	<td></td>
+	<td><a href="#"><img src="icons/pencil-writing-on-a-paper-sheet.png"></a></td>
+	<td><a href="#"><img src="icons/checklist.png"></a></td>
+</tr>
+<tr id="fonts">
+	<td>Ανταλλαγή συγγραμμάτων</td>
+	<td></td>
+	<td>Παραγγελία συγγραμμάτων</td>
+	<td>Εισαγωγή προτεινόμενων συγγραμμάτων</td>
+</tr>
+-->
 <!--
 <div>
 	<input type="button" value="skata">
