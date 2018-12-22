15e4460326cc0e6b712c3de2a9cdf264375539c9
Nikolas Lep <nikolaslep_@hotmail.com>
2018-12-22 20:07:10 +0200

    small change on telephone in footer

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 		<div><img src="icons/two-black-rectangular-speech-bubbles-interface-chat-symbol.png"></div>
 		<div><a href="#" >Ηλ. Φόρμα Επικοινωνίας</a></div>
 		<div><img src="icons/envelope.png"></div>
-		<div><a>210 2430436</a></div>
+		<div><a href="tel:+302102430436">210 2430436</a></div>
 		<div><img src="icons/call-answer.png"></div>
 	</div>
 </div>
