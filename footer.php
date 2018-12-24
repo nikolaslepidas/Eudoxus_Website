bd129eee71890cf410147e25e2ad3a540e35bcea
ejernity <nikoslamprinos13@gmail.com>
2018-12-24 18:05:00 +0200

    validate registration/error messages for empty fields

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div class="wrapper">
 	<div id="footer">
 		<div><a href="#" >Συχνές Ερωτήσεις</a></div>
-		<div><img src="icons/two-black-rectangular-speech-bubbles-interface-chat-symbol.png"  alt="Συχνές Ερωτήσεις"></div>
+		<div><img src="icons/chat.png"  alt="Συχνές Ερωτήσεις"></div>
 		<div><a href="#" >Ηλ. Φόρμα Επικοινωνίας</a></div>
 		<div><img src="icons/envelope.png"  alt="Ηλ. Φόρμα Επικοινωνίας"></div>
 		<div><a href="tel:+302102430436">210 2430436</a></div>
