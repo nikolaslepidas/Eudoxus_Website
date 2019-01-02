811d4c94812203ba31ac28dde7b4042a737e2449
ejernity <nikoslamprinos13@gmail.com>
2019-01-03 00:45:00 +0200

    footer link for communication link user to communication_form page

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<div id="footer">
 		<div><a href="#" >Συχνές Ερωτήσεις</a></div>
 		<div><img src="icons/chat.png"  alt="Συχνές Ερωτήσεις"></div>
-		<div><a href="#" >Ηλ. Φόρμα Επικοινωνίας</a></div>
+		<div><a href="./communication_form.php" >Ηλ. Φόρμα Επικοινωνίας</a></div>
 		<div><img src="icons/envelope.png"  alt="Ηλ. Φόρμα Επικοινωνίας"></div>
 		<div><a href="tel:+302102430436">210 2430436</a></div>
 		<div><img src="icons/call-answer.png"  alt="Τηλέφωνο"></div>
