fd7d0b2830708a11dc91d54adee30c31f127e3de
ejernity <nikoslamprinos13@gmail.com>
2019-01-05 23:22:32 +0200

    front end completed_change colors of grid

--- edit_profile.php
+++ edit_profile.php
@@ -33,20 +33,33 @@
 
                     <label for="edit_profile_email">Νέο e-mail</label>
                     <input type="email" name="profile_email_to_change" placeholder="Εισαγωγή e-mail" id="edit_profile_email">
-<!---->
+
                     <label for="old_profile_password">Παλιό συνθηματικό</label>
                     <input type="password" name="old_profile_password" placeholder="Εισαγωγή παλιού συνθηματικού" id="old_profile_password">
 
                     <label for="new_profile_password">Νέο συνθηματικό</label>
                     <input type="password" name="new_profile_password" placeholder="Εισαγωγή νέου συνθηματικού" id="new_profile_password">
 
-                </div>
+					<div id="edit_publisher_fields_for_profile" style="display: none;">
+
+						<label for="edit_profile_publisher_name">Όνομα εκδοτικού οίκου</label>
+						<input type="text" name="edit_profile_publisher_name" placeholder="Εισαγωγή ονόματος εκδοτικού οίκου" id="edit_profile_publisher_name">
+
+						<label for="edit_profile_publisher_city">Πόλη εκδοτικού οίκου</label>
+						<input type="text" name="edit_profile_publisher_city" placeholder="Εισαγωγή πόλης εκδοτικού οίκου" id="edit_profile_publisher_city">
+
+						<label for="profile_publisher_phone">Τηλέφωνο εκδοτικού οίκου</label>
+						<input type="text" name="edit_profile_publisher_phone" placeholder="Εισαγωγή τηλεφώνου εκδοτικού οίκου" id="edit_profile_publisher_phone">
+
+					</div>
+
+				</div>
 
                 <button type="submit" name="edit_profile">Αλλαγή</button>
 
             </form>
             
-		</div>
+        </div>
 
 	</div>
 
