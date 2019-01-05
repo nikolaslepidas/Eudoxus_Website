3b8efdca34cd19091fe5589f53fb3fe31d9b16a8
Nikolas Lep <nikolaslep_@hotmail.com>
2019-01-05 18:29:41 +0200

    fornt-end for search.php almost ready

--- search.php
+++ search.php
@@ -9,11 +9,11 @@
 		<link rel="stylesheet" href="css/communication_form.css">
 
 		<title>Εύδοξος - Αναζήτηση</title>
-
+        
 	</head>
 	<body>
-
-	<?php require_once './header.php' ?>
+        
+        <?php require_once './header.php' ?>
 	<div class="wrapper">
 		<ul class="breadcrumb">
 			<li><a href="index.php">Αρχική</a></li>
@@ -25,6 +25,52 @@
 		</ul>
 	</div>
 
+
+    <div class="container_com_form">
+    <table>
+        <tbody>
+        <tr>
+            <th>
+                Τίτλος:
+            </th>
+            <td>
+                <input name="uname" type="text" id="unameid" style="width:88%;">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                Συγγραφέας:
+            </th>
+            <td>
+                <input name="uname" type="text" id="telid" style="width:88%;">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                Εκδόσεις:
+            </th>
+            <td>
+                <input name="uname" type="text" id="emailid" style="width:88%;">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                ISBN:
+            </th>
+            <td>
+                <input name="uname" type="text" id="emailid" style="width:88%;">
+            </td>
+        </tr>
+        <tr>
+            <th></th>
+            <td>
+                <button type="submit">Αναζήτηση</button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+    
 
 	<?php require_once './footer.php' ?>
 
