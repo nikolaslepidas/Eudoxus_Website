f50f31c347228dd0ad52a48e4fc33bcc7c61f6f1
Nikolas Lep <nikolaslep_@hotmail.com>
2019-01-07 13:00:02 +0200

    created dynamic queries for search_books.php

--- search_books.php
+++ search_books.php
@@ -26,15 +26,69 @@
 	</div>
 
 
+<?php
+ // Connect to the database
+ require_once 'mysql_connector.php';
+
+if (isset($_POST['book_search'])){
+
+    if ((isset($_POST['book_title']))){
+        $book_title = $_POST['book_title'];
+        if (empty($book_title)){
+            $book_title = "%";
+        }
+    }
+
+    if (isset($_POST['book_writer'])){
+        $book_writer = $_POST['book_writer'];
+        if (empty($book_writer)){
+            $book_writer = "%";
+        }
+    }
+
+    
+    if (isset($_POST['book_publisher'])){
+        $book_publisher = $_POST['book_publisher'];
+        if (empty($book_publisher)){
+            $book_publisher = "%";
+        }
+    }
+
+    if (isset($_POST['book_isbn'])){
+        $book_isbn = $_POST['book_isbn'];
+        if (empty($book_isbn)){
+            $book_isbn = "%";
+        }
+    }
+
+    $query = "select * from book where title like '$book_title' and isbn like '$book_isbn' 
+    and writer like '$book_writer' and publisher like '$book_publisher';";
+
+    echo $query;
+    $result=mysqli_query($connection,$query);
+    //echo var_dump($result);
+    mysqli_data_seek($result, 0);
+    $row = mysqli_fetch_assoc($result);
+    echo $row['title'];
+    echo $row['isbn'];
+    echo $row['publisher'];
+    echo $row['writer'];
+
+}
+
+?>
+
+
     <div class="container_com_form">
     <table>
         <tbody>
+        <form id="book_search_form" method="post">
         <tr>
             <th>
                 Τίτλος:
             </th>
             <td>
-                <input name="uname" type="text" id="unameid" style="width:88%;">
+                <input name="book_title" type="text" id="unameid" style="width:88%;">
             </td>
         </tr>
         <tr>
@@ -42,7 +96,7 @@
                 Συγγραφέας:
             </th>
             <td>
-                <input name="uname" type="text" id="telid" style="width:88%;">
+                <input name="book_writer" type="text" id="telid" style="width:88%;">
             </td>
         </tr>
         <tr>
@@ -50,7 +104,7 @@
                 Εκδόσεις:
             </th>
             <td>
-                <input name="uname" type="text" id="emailid" style="width:88%;">
+                <input name="book_publisher" type="text" id="emailid" style="width:88%;">
             </td>
         </tr>
         <tr>
@@ -58,13 +112,14 @@
                 ISBN:
             </th>
             <td>
-                <input name="uname" type="text" id="emailid" style="width:88%;">
+                <input name="book_isbn" type="text" id="emailid" style="width:88%;">
             </td>
         </tr>
+        </form>
         <tr>
             <th></th>
             <td>
-                <button type="submit">Αναζήτηση</button>
+                <button type="submit" name="book_search" form="book_search_form">Αναζήτηση</button>
             </td>
         </tr>
         </tbody>
