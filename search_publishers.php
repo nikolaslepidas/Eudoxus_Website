9291738aa551636bc22a45f7ed1a9f1b5a7e1627
Nikolas Lep <nikolaslep_@hotmail.com>
2019-01-07 13:25:33 +0200

    search_publishers is ready in the backend

--- search_publishers.php
+++ search_publishers.php
@@ -25,30 +25,72 @@
 		</ul>
 	</div>
 
+    <?php
+ // Connect to the database
+ require_once 'mysql_connector.php';
+
+if (isset($_POST['publisher_search'])){
+
+    if ((isset($_POST['brand_name']))){
+        $brand_name= $_POST['brand_name'];
+        if (empty($brand_name)){
+            $brand_name= "%";
+        }
+    }
+
+    if (isset($_POST['city'])){
+        $city = $_POST['city'];
+        if (empty($city)){
+            $city = "%";
+        }
+    }
+
+    if (!((strcmp($brand_name,"%") == 0) && (strcmp($city,"%") == 0))){
+
+        $query = "select * from publisher where brand_name like '$brand_name' and city like '$city';";
+    
+        echo $query;
+        $result=mysqli_query($connection,$query);
+        //echo var_dump($result);
+        mysqli_data_seek($result, 0);
+    
+        for($i=0;$i<mysqli_num_rows($result);$i++){
+            $row = mysqli_fetch_assoc($result);
+            echo $row['brand_name'];
+            echo $row['city'];
+            echo $row['phone'];
+            echo $row['user_email'];
+        }
+    }
+}
+
+?>
 
     <div class="container_com_form">
     <table>
         <tbody>
+        <form id="publisher_search_form" method="post">
         <tr>
             <th>
                 Επωνυμία:
             </th>
             <td>
-                <input name="uname" type="text" id="unameid" style="width:88%;">
+                <input name="brand_name" type="text" id="unameid" style="width:88%;">
             </td>
         </tr>
         <tr>
             <th>
-                Έδρα:
+                Πόλη:
             </th>
             <td>
-                <input name="uname" type="text" id="telid" style="width:88%;">
+                <input name="city" type="text" id="telid" style="width:88%;">
             </td>
         </tr>
+        </form>
         <tr>
             <th></th>
             <td>
-                <button type="submit">Αναζήτηση</button>
+                <button type="submit" name="publisher_search" form="publisher_search_form">Αναζήτηση</button>
             </td>
         </tr>
         </tbody>
