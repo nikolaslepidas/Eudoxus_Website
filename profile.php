c9b4ad2198827f2c97524034548dfdb37b0ff5a0
ejernity <nikoslamprinos13@gmail.com>
2019-01-08 17:26:56 +0200

    first try onclick edit username

--- profile.php
+++ profile.php
@@ -36,22 +36,22 @@
 			$row = mysqli_fetch_assoc($result);
 
 			if ($row['userGroup'] == 'student') {
-				$row['userGroup'] = 'Φοιτητής';
+				$userGroup = 'Φοιτητής';
 				echo "
 				<img src='./icons/student.png' alt='user' title='user' style='display: block;' id='student_img_profile'/>
 				";
 			} else if ($row['userGroup'] == 'publisher') {
-				$row['userGroup'] = 'Εκδότης';
+				$userGroup = 'Εκδότης';
 				echo "
 				<img src='./icons/manager.png' alt='user' title='user' style='display: block;' id='publisher_img_profile'/>
 				";
 			} else if ($row['userGroup'] == 'distributor') {
-				$row['userGroup'] = 'Διανομέας';
+				$userGroup = 'Διανομέας';
 				echo "
 				<img src='./icons/book.png' alt='user' title='user' style='display: block;' id='book_img_profile'/>
 				";
 			} else if ($row['userGroup'] == 'sectretary') {
-				$row['userGroup'] = 'Γραμματεία';
+				$userGroup = 'Γραμματεία';
 				echo "
 				<img src='./icons/secretary.png' alt='user' title='user' style='display: block;' id='secretary_img_profile'/>
 				";
@@ -59,32 +59,76 @@
 		?>
 			<div class="user_fields">
 
-				<label for="profile_username">Όνομα χρήστη:</label>
+			<table>
+				<tbody>
+				<form method='post'>
+				<tr>
+				<td>
+					<label for="new_username">Όνομα χρήστη:</label>
+				</td>
+				<td>
+					<?php
+					if (isset($_POST['edit_username'])) {		// an patithei to epeksergasia
+						echo "
+						<input name='new_username' placeholder='$row[username]'/>
+						</td>
+						<td>
+							<button type='submit' name='change_username' id='change_username'>Αλλαγή</button>
+						</td>
+						";
+					} else {
+						echo "
+						<p name='profile_username' id='profile_username'>$row[username]</p>
+						</td>
+						<td>
+							<button type='submit' name='edit_username' id='edit_username'>Επεξεργασία</button>
+						</td>
+						";
+					}
+					if (isset($_POST['change_username'])) {		// an patithei to allagi
+						$new_username = $_POST['new_username'];
+						$query = "
+						SET SQL_SAFE_UPDATES = 0;
+						UPDATE user SET username = '$new_username' where email = '$row[email]';
+						";
+						$result_edit_username = mysqli_query($connection, $query);
+					}
+					?>
+				</tr>
+
+				<tr>
+				<td>
+					<label for="profile_email">E-mail:</label>
+				</td>
+				<td>
+					<?php
+						echo "
+						<p name='profile_email' id='profile_email'>$row[email]</p>
+						";
+					?>
+				</td>
+				<td>
+					<button type="submit" name="edit_email">Επεξεργασία</button>
+				</td>
+				</tr>
+
+				<tr>
+				<td>
+					<label for="profile_userGroup">Ομάδα χρήστη:</label>
+				</td>
+				<td>
+					<?php
+						echo "
+						<p name='profile_userGroup' id='profile_userGroup'>$userGroup</p>					
+						";
+					?>
+				</td>
+				<td></td>
+				</tr>
+
 				<?php
-					echo "
-					<span name='profile_username' id='profile_username'> $row[username] </span><br><br>					
-					";
-				?>
 
-				<label for="profile_email">E-mail:</label>
-				<?php
-					echo "
-					<span name='profile_email' id='profile_email'> $row[email] </span><br><br>
-					";
-				?>
-				
-
-				<label for="profile_userGroup">Ομάδα χρήστη:</label>
-
-				<?php
-					echo "
-					<span name='profile_userGroup' id='profile_userGroup'> $row[userGroup] </span><br><br>					
-					";
-				?>
-
-				<?php
-
-					if ($row['userGroup'] == 'Φοιτητής') {
+					if ($userGroup == 'Φοιτητής') {
 						$query = "select * from student where user_email = '$_SESSION[user_email]'";
 						$result=mysqli_query($connection,$query);
 						mysqli_data_seek($result,0);
@@ -92,17 +136,27 @@
 
 						echo "
 						<div id='student_fields_for_profile'>
-
-						<label for='profile_university'>Πανεπιστήμιο:</label>
+						
+						<tr>
+						<td>
+							<label for='profile_university'>Πανεπιστήμιο:</label>
+						</td>
 						";
 
 						if ($row['university'] == 'ekpa') {
 							$row['university'] = 'ΕΚΠΑ';
 
 							echo "
-							<span name='profile_university' id='profile_university'> $row[university] </span><br><br>
+							<td>
+								<p name='profile_university' id='profile_university'>$row[university]</p>
+							</td>
+							<td></td>
+							</tr>
 	
-							<label for='profile_department'>Τμήμα:</label>
+							<tr>
+							<td>
+								<label for='profile_department'>Τμήμα:</label>
+							</td>
 							";
 
 							if ($row['department'] == 'computer') {
@@ -114,12 +168,15 @@
 							}
 
 							echo "
-							<span name='profile_department' id='profile_department'> $row[department] </span><br><br>
-		
+							<td>
+								<p name='profile_department' id='profile_department'>$row[department]</p>
+							</td>
+							<td></td>
+							</tr>
 							</div>
 							";
 						}
-					} else if ($row['userGroup'] == 'Εκδότης') {
+					} else if ($userGroup == 'Εκδότης') {
 						$query = "select * from publisher where user_email = '$_SESSION[user_email]'";
 						$result=mysqli_query($connection,$query);
 						mysqli_data_seek($result,0);
@@ -141,10 +198,14 @@
 						";
 					}
 				?>
+				</form>
+				</tbody>
+
+			</table>
 
 			</div>
 
-			<div class="edit_profile_button">
+			<div class="edit_profile_button" style="display: none;">
 				<a href="./edit_profile.php"><button type="button">Επεξεργασία</button></a>
 			</div>
 
