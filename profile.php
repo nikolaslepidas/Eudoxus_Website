856b1e30da297a5d22bd96de4035999457eb5048
ejernity <nikoslamprinos13@gmail.com>
2019-01-08 13:43:46 +0200

    changes on offset of row variable

--- profile.php
+++ profile.php
@@ -30,29 +30,28 @@
 			require_once 'mysql_connector.php';
 
 			// SQL query to fetch information of registerd users and finds user match.
-			$query = "select userGroup from user where email='$_SESSION[user_email]'";
+			$query = "select * from user where email='$_SESSION[user_email]'";
 			$result=mysqli_query($connection,$query);
 			mysqli_data_seek($result,0);
 			$row = mysqli_fetch_assoc($result);
-			$userGroup = implode($row);
 
-			if ($userGroup == 'student') {
-				$userGroup = 'Φοιτητής';
+			if ($row['userGroup'] == 'student') {
+				$row['userGroup'] = 'Φοιτητής';
 				echo "
 				<img src='./icons/student.png' alt='user' title='user' style='display: block;' id='student_img_profile'/>
 				";
-			} else if ($userGroup == 'publisher') {
-				$userGroup = 'Εκδότης';
+			} else if ($row['userGroup'] == 'publisher') {
+				$row['userGroup'] = 'Εκδότης';
 				echo "
 				<img src='./icons/manager.png' alt='user' title='user' style='display: block;' id='publisher_img_profile'/>
 				";
-			} else if ($userGroup == 'distributor') {
-				$userGroup = 'Διανομέας';
+			} else if ($row['userGroup'] == 'distributor') {
+				$row['userGroup'] = 'Διανομέας';
 				echo "
 				<img src='./icons/book.png' alt='user' title='user' style='display: block;' id='book_img_profile'/>
 				";
-			} else if ($userGroup == 'sectretary') {
-				$userGroup = 'Γραμματεία';
+			} else if ($row['userGroup'] == 'sectretary') {
+				$row['userGroup'] = 'Γραμματεία';
 				echo "
 				<img src='./icons/secretary.png' alt='user' title='user' style='display: block;' id='secretary_img_profile'/>
 				";
@@ -62,20 +61,16 @@
 
 				<label for="profile_username">Όνομα χρήστη:</label>
 				<?php
-					$query = "select username from user where email='$_SESSION[user_email]'";
-					$result=mysqli_query($connection,$query);
-					mysqli_data_seek($result,0);
-					$row = mysqli_fetch_assoc($result);
-					$username = implode($row);
-
 					echo "
-					<span name='profile_username' id='profile_username'> $username </span><br><br>					
+					<span name='profile_username' id='profile_username'> $row[username] </span><br><br>					
 					";
 				?>
 
 				<label for="profile_email">E-mail:</label>
 				<?php
-					echo "<span name='profile_email' id='profile_email'> $_SESSION[user_email] </span><br><br>";
+					echo "
+					<span name='profile_email' id='profile_email'> $row[email] </span><br><br>
+					";
 				?>
 				
 
@@ -83,17 +78,17 @@
 
 				<?php
 					echo "
-					<span name='profile_userGroup' id='profile_userGroup'> $userGroup </span><br><br>					
+					<span name='profile_userGroup' id='profile_userGroup'> $row[userGroup] </span><br><br>					
 					";
 				?>
 
 				<?php
 
-					if ($userGroup == 'Φοιτητής') {
-						$query = "select * from user u,student s where u.email = '$_SESSION[user_email]' && s.user_email = '$_SESSION[user_email]'";
+					if ($row['userGroup'] == 'Φοιτητής') {
+						$query = "select * from student where user_email = '$_SESSION[user_email]'";
 						$result=mysqli_query($connection,$query);
 						mysqli_data_seek($result,0);
-						$row = mysqli_fetch_row($result);
+						$row = mysqli_fetch_assoc($result);
 
 						echo "
 						<div id='student_fields_for_profile'>
@@ -101,46 +96,46 @@
 						<label for='profile_university'>Πανεπιστήμιο:</label>
 						";
 
-						if ($row[5] == 'ekpa') {
-							$row[5] = 'ΕΚΠΑ';
+						if ($row['university'] == 'ekpa') {
+							$row['university'] = 'ΕΚΠΑ';
 
 							echo "
-							<span name='profile_university' id='profile_university'> $row[5] </span><br><br>
+							<span name='profile_university' id='profile_university'> $row[university] </span><br><br>
 	
 							<label for='profile_department'>Τμήμα:</label>
 							";
 
-							if ($row[6] == 'computer') {
-								$row[6] = 'Πληροφορικής & Τηλεπικοινωνιών';
-							} else if ($row[6] == 'maths') {
-								$row[6] = 'Μαθηματικών';
-							} else if ($row[6] == 'chemistry') {
-								$row[6] = 'Χημείας';
+							if ($row['department'] == 'computer') {
+								$row['department'] = 'Πληροφορικής & Τηλεπικοινωνιών';
+							} else if ($row['department'] == 'maths') {
+								$row['department'] = 'Μαθηματικών';
+							} else if ($row['department'] == 'chemistry') {
+								$row['department'] = 'Χημείας';
 							}
 
 							echo "
-							<span name='profile_department' id='profile_department'> $row[6] </span><br><br>
+							<span name='profile_department' id='profile_department'> $row[department] </span><br><br>
 		
 							</div>
 							";
 						}
-					} else if ($userGroup == 'Εκδότης') {
-						$query = "select * from user u,publisher s where u.email = '$_SESSION[user_email]' && s.user_email = '$_SESSION[user_email]'";
+					} else if ($row['userGroup'] == 'Εκδότης') {
+						$query = "select * from publisher where user_email = '$_SESSION[user_email]'";
 						$result=mysqli_query($connection,$query);
 						mysqli_data_seek($result,0);
-						$row = mysqli_fetch_row($result);
+						$row = mysqli_fetch_assoc($result);
 
 						echo "
 						<div id='publisher_fields_for_profile'>
 	
 						<label for='profile_publisher_name'>Όνομα εκδοτικού οίκου:</label>
-						<span name='profile_publisher_name' id='profile_publisher_name'> $row[5] </span><br><br>
+						<span name='profile_publisher_name' id='profile_publisher_name'> $row[brand_name] </span><br><br>
 	
 						<label for='profile_publisher_city'>Πόλη εκδοτικού οίκου:</label>
-						<span name='profile_publisher_city' id='profile_publisher_city'> $row[6] </span><br><br>
+						<span name='profile_publisher_city' id='profile_publisher_city'> $row[city] </span><br><br>
 	
 						<label for='profile_publisher_phone'>Τηλέφωνο εκδοτικού οίκου:</label>
-						<span name='profile_publisher_phone' id='profile_publisher_phone'> $row[7] </span><br><br>
+						<span name='profile_publisher_phone' id='profile_publisher_phone'> $row[phone] </span><br><br>
 	
 						</div>
 						";
