a1c796120250c5271b1d70b4166c1341bdd7a7fc
ejernity <nikoslamprinos13@gmail.com>
2019-01-08 14:37:15 +0200

    extra fields for publisher on edit

--- edit_profile.php
+++ edit_profile.php
@@ -7,6 +7,7 @@
 		<link rel="stylesheet" href="css/base.css">
 		<link rel="stylesheet" href="css/breadcrumbs.css">
 		<link rel="stylesheet" href="css/profile.css">
+		<link rel="stylesheet" href="css/bookOrder.css">
 
 		<title>Εύδοξος - Επεξεργασία προφίλ</title>
 
@@ -21,6 +22,21 @@
 			<li>Επεξεργασία προφίλ</li> 
 		</ul>
 	</div>
+
+	<?php
+	// Connect to the database
+	require_once 'mysql_connector.php';
+
+	if(isset($_POST['edit_profile'])) {
+
+		if($_POST['profile_email_to_change'] != "") {
+			$new_email = $_POST['profile_email_to_change'];
+		}
+
+	}
+	
+	?>
+<div class="padding_needed_for_footer_edit_profile">
 
 	<div class="container_grid_profile">
 
@@ -40,18 +56,34 @@
                     <label for="new_profile_password">Νέο συνθηματικό</label>
                     <input type="password" name="new_profile_password" placeholder="Εισαγωγή νέου συνθηματικού" id="new_profile_password">
 
-					<div id="edit_publisher_fields_for_profile" style="display: none;">
+					<?php
+					
+					if (isset($_SESSION['user_email'])) {
+						$query = "select userGroup from user where email = '$_SESSION[user_email]'";
+						$result=mysqli_query($connection,$query);
+						mysqli_data_seek($result,0);
+						$row = mysqli_fetch_assoc($result);
+						$userGroup = implode($row);
 
-						<label for="edit_profile_publisher_name">Όνομα εκδοτικού οίκου</label>
-						<input type="text" name="edit_profile_publisher_name" placeholder="Εισαγωγή ονόματος εκδοτικού οίκου" id="edit_profile_publisher_name">
+						if($userGroup == 'publisher') {
+							echo "
+							<div id='edit_publisher_fields_for_profile'>
 
-						<label for="edit_profile_publisher_city">Πόλη εκδοτικού οίκου</label>
-						<input type="text" name="edit_profile_publisher_city" placeholder="Εισαγωγή πόλης εκδοτικού οίκου" id="edit_profile_publisher_city">
-
-						<label for="profile_publisher_phone">Τηλέφωνο εκδοτικού οίκου</label>
-						<input type="text" name="edit_profile_publisher_phone" placeholder="Εισαγωγή τηλεφώνου εκδοτικού οίκου" id="edit_profile_publisher_phone">
-
-					</div>
+							<label for='edit_profile_publisher_name'>Όνομα εκδοτικού οίκου</label>
+							<input type='text' name='edit_profile_publisher_name' placeholder='Εισαγωγή ονόματος εκδοτικού οίκου' id='edit_profile_publisher_name'>
+	
+							<label for='edit_profile_publisher_city'>Πόλη εκδοτικού οίκου</label>
+							<input type='text' name='edit_profile_publisher_city' placeholder='Εισαγωγή πόλης εκδοτικού οίκου' id='edit_profile_publisher_city'>
+	
+							<label for='profile_publisher_phone'>Τηλέφωνο εκδοτικού οίκου</label>
+							<input type='text' name='edit_profile_publisher_phone' placeholder='Εισαγωγή τηλεφώνου εκδοτικού οίκου' id='edit_profile_publisher_phone'>
+	
+							</div>
+							";
+						}
+					}
+					
+					?>
 
 				</div>
 
@@ -62,6 +94,8 @@
         </div>
 
 	</div>
+
+</div>
 
 	<?php require_once './footer.php' ?>
 
