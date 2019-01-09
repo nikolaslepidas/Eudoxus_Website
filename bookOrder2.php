2a562b4c42b33c817735f1a3a5e861135aad0288
Nikolas Lep <nikolaslep_@hotmail.com>
2019-01-09 12:21:18 +0200

    took bookOrder2.php from branch bookOrder2 where I was working

--- bookOrder2.php
+++ bookOrder2.php
@@ -1,29 +1,36 @@
 <?php 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $book1 = $_POST['Grammiki_Algebra']; 
-        $book2 = $_POST['Diakrita_Mathimatika']; 
-        $book3 = $_POST['Analisi_II'];
-        echo $book1;
-        echo $book2;
-        echo $book3;
-    }else
+        $courses = array("grammiki_algebra","diakrita_mathimatika","eisagogi_stin_pliroforiki_kai_stis_tilepikoinonies","eisagogi_ston_programmatismo","logiki_sxediasi");
+        $courses_length = count($courses);
+        $checked_courses = array();
+
+        for ($i=0;$i<$courses_length;$i++){
+            if (isset($_POST[$courses[$i]])){
+                array_push($checked_courses,$_POST[$courses[$i]]);
+            }
+        }
+        $checked_courses_length = count($checked_courses);
+
+        for ($j=0;$j<$checked_courses_length;$j++){
+            echo $checked_courses[$j];
+            echo "~";
+            require_once './mysql_connector.php';
+            
+            $course_info_from_db=mysqli_query($connection,"select * from course where title='$checked_courses[$j]';");
+            echo "select * from course where title='$checked_courses[$j]';";
+            mysqli_data_seek($course_info_from_db,0);
+            $row = mysqli_fetch_assoc($course_info_from_db);
+            $query="select * from book, course_has_book, course where isbn=book_isbn and course_idcourse=$row[idcourse];";
+            echo $query;
+            $books_for_course=mysqli_query($connection,$query);
+            mysqli_data_seek($books_for_course,0);
+            $row = mysqli_fetch_assoc($books_for_course);
+            echo var_dump($row);
+        }
+    }else{
         echo "no post request got here";
         echo var_dump($_POST);
-
-
-
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $book1 = $_GET['Grammiki_Algebra']; 
-            $book2 = $_GET['Diakrita_Mathimatika']; 
-            $book3 = $_GET['Analisi_II'];
-            echo $book1;
-            echo $book2;
-            echo $book3;
-            echo var_dump($_GET);
-            
-            // The request is using the POST method
-        }else
-        echo "no post request got here";
+    }
 ?>
 
 
