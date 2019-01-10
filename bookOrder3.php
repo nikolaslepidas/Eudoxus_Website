35c991e353cc2cf1381c10a7265322eeca7ebece
ejernity <nikoslamprinos13@gmail.com>
2019-01-10 15:47:40 +0200

    breadcrumb on book order

--- bookOrder3.php
+++ bookOrder3.php
@@ -17,12 +17,9 @@
 	<?php require_once './header.php' ?>
 	<div class="wrapper">
 		<ul class="breadcrumb">
-			<li><a href="index.php">Αρχική</a></li>
-			<!--
-			<li><a href="#">Page2</a></li>
-			<li><a href="#">Page3</a></li>
-			-->
-			<li>Δήλωση συγγραμμάτων</li> 
+            <li><a href="index.php">Αρχική</a></li>
+            <li><a href="student.php">Φοιτητής</a></li>
+			<li>Δήλωση συγγραμμάτων</li>
 		</ul>
 	</div>
 
