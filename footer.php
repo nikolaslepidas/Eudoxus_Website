51903d98646c516f1cff2a91140b1d0ed22ff24b
ejernity <nikoslamprinos13@gmail.com>
2019-01-10 17:04:07 +0200

    old footer v2

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <!--	Footer	-->
-<div class="wrapper-footer">
-	<div id="footer">
-		<div><a href="#" >Συχνές Ερωτήσεις</a></div>
-		<div><img src="icons/chat.png"  alt="Συχνές Ερωτήσεις"></div>
-		<div><a href="./communication_form.php" >Ηλ. Φόρμα Επικοινωνίας</a></div>
-		<div><img src="icons/envelope.png"  alt="Ηλ. Φόρμα Επικοινωνίας"></div>
-		<div><a href="tel:+302102430436">210 2430436</a></div>
-		<div><img src="icons/call-answer.png"  alt="Τηλέφωνο"></div>
+<div class="container_grid_for_footer ">
+	<div class="footer_button">
+		<a href="#" >Συχνές Ερωτήσεις</a>
+		<img src="icons/chat.png"  alt="Συχνές Ερωτήσεις">
+		<a href="./communication_form.php" >Ηλ. Φόρμα Επικοινωνίας</a>
+		<img src="icons/envelope.png"  alt="Ηλ. Φόρμα Επικοινωνίας">
+		<a href="tel:+302102430436">210 2430436</a>
+		<img src="icons/call-answer.png"  alt="Τηλέφωνο">
 	</div>
 </div>
 <!--	End of Footer	-->
