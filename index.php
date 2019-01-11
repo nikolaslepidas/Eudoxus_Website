4312dc8505e1e42d297c627e2fda8ff20710252b
niklepgit <nikolaslep_@hotmail.com>
2019-01-11 15:56:17 +0200

    add comments into css files. Also changed home.css to index.css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
 		<link rel="stylesheet" href="css/base.css">
-		<link rel="stylesheet" href="css/home.css">
+		<link rel="stylesheet" href="css/index.css">
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		
